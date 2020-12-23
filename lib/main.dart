0539bc21017be8aba2220515be405c6234e2167d
Michellejacob001 <michelle.jacob002@gmail.com>
2020-12-23 13:30:48 +0530

    initial changes'

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+import 'quiz_brain.dart';
 
+QuizBrain quizBrain = QuizBrain();
+
+class QuizBrain {
+}
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,6 +31,27 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+void checkAnswer(bool userPickedAnswer){
+  bool correctanswer = quizBrain.getCorrectAnswer();
+  setState(() {
+  if (userPickedAnswer==correctanswer)
+  {
+ scoreKeeper.add(Icon(
+   Icons.check,
+   color: Colors.green,
+ ),);
+  }
+  else
+    scoreKeeper.add(Icon(
+      Icons.close,
+      color: Colors.red,
+    ),);
+
+    quizBrain.nextQuestion();
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +64,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText();
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +88,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+checkAnswer(true);
+
               },
             ),
           ),
@@ -79,16 +107,22 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+               checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
+
+
+
+
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
